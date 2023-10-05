1eddb4eebdebd1b289971c993a2ab064ce0e7bad
costa86 <costa86@pm.me>
2023-10-05 15:46:10 +0100

    auth

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"os"
 	"sync"
 
+	"github.com/go-git/go-git/v5/plumbing/transport/http"
 	"gopkg.in/src-d/go-git.v4"
 )
 
@@ -26,22 +27,36 @@ func getRepos(repoFile string) []Repo {
 	return repos
 }
 
-func downloadRepo(repo Repo) {
-	_, err := git.PlainClone(repo.Folder, false, &git.CloneOptions{URL: repo.Url, Progress: os.Stdout})
+func downloadRepo(repo Repo, username string, password string) {
+	auth := &http.BasicAuth{
+		Username: username,
+		Password: password,
+	}
+	_, err := git.PlainClone(repo.Folder, false, &git.CloneOptions{URL: repo.Url, Progress: os.Stdout, Auth: auth})
 	if err != nil {
 		print(err)
 		os.Exit(1)
 	}
 }
 
+// func downloadRepo(repo Repo) {
+// 	_, err := git.PlainClone(repo.Folder, false, &git.CloneOptions{URL: repo.Url, Progress: os.Stdout})
+// 	if err != nil {
+// 		print(err)
+// 		os.Exit(1)
+// 	}
+// }
+
 func main() {
 
 	args := os.Args
-	if len(args) != 2 {
+	if len(args) != 4 {
 		print("JSON file is missing. File structure is: [{\"folder\":\"folderName\":\"url\":\"repoUrl\"}]")
 		os.Exit(1)
 	}
 	jsonFile := args[1]
+	username := args[2]
+	password := args[3]
 
 	projects := getRepos(jsonFile)
 	var wg sync.WaitGroup
@@ -51,7 +66,7 @@ func main() {
 		v := v
 		i := i + 1
 		go func() {
-			downloadRepo(v)
+			downloadRepo(v, username, password)
 			print(i, "/", len(projects), ":", v.Url, "downloaded to", v.Folder)
 			defer wg.Done()
 		}()
